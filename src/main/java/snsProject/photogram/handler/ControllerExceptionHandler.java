487915a53a4550b934f3e4fb927d594bd8e242a9
rnwnsgud <rnwnsgud90@naver.com>
2022-10-22 00:57:24 +0900

    회원가입 완료

--- src/main/java/snsProject/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/snsProject/photogram/handler/ControllerExceptionHandler.java
@@ -1,21 +1,25 @@
 package snsProject.photogram.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import snsProject.photogram.dto.CMRespDto;
 import snsProject.photogram.handler.exception.CustomValidationException;
+import snsProject.photogram.script.Script;
 
 import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ExceptionHandler(RuntimeException.class)
-//    public String validationException(RuntimeException e) {
-//        return e.getMessage();
+//    @ExceptionHandler(CustomValidationException.class)
+//    public ResponseEntity<?> validationException(CustomValidationException e) {
+//        return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
 //    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    @ExceptionHandler(CustomValidationException.class)
+    public String validationException(CustomValidationException e) {
+        return Script.back(e.getErrorMap().toString());
     }
 }
